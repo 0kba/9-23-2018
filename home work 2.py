e56e2a065bb2cd76986200db786a1738db153ff3
okba <43217617+0kba@users.noreply.github.com>
2018-09-23 15:55:49 -0500

    Update home work 2.py

--- home work 2.py
+++ home work 2.py
@@ -2,3 +2,9 @@ list = [1, "learning python", [], object() ]
 for int in list:
     thekind = type (int)
     print(f" (( {int } )) type is { thekind}")
+    
+result__________________________________________
+ (( 1 )) type is <class 'int'>
+ (( learning python )) type is <class 'str'>
+ (( [] )) type is <class 'list'>
+ (( <object object at 0x000002114E01E0C0> )) type is <class 'object'>
