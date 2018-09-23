08dbf1e6083d527697cabedff279348cf6f648e1
okba <43217617+0kba@users.noreply.github.com>
2018-09-23 15:44:29 -0500

    Add files via upload

---
+++ home work 1.py
@@ -0,0 +1,18 @@
+numbers = [1,2,2,2,3,3,4,4,4,4,5,6,6,-1,0,1,2,5]
+count=0
+for num in numbers:
+    if num == 2:
+        count= count +1
+print (f"number 2 appeared {count} times.")
+
+for num in numbers:
+    if num == 3:
+        count= count +1
+print (f"number 3 appeared {count} times.")
+
+for num in numbers:
+    if num == 4:
+        count= count +1
+print (f"number 4 appeared {count} times.")
+
+
---
+++ home work 2.py
@@ -0,0 +1,4 @@
+list = [1, "learning python", [], object() ]
+for int in list:
+    thekind = type (int)
+    print(f" (( {int } )) type is { thekind}")
