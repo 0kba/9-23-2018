f5cb7a86ce9f27abc950f907dd8b539c1bc62708
okba <43217617+0kba@users.noreply.github.com>
2018-09-24 21:47:06 -0500

    Update and rename homework1.py to if num divisble.py

---
+++ if num divisble.py
@@ -0,0 +1,79 @@
+numbers = [3,5,10,9,45,23,90,7,98,89,32,54,65,76,87,12,21,11,22,33,44,55,66,77,88,99]
+
+for num in numbers:
+
+    if num%3 == 0:
+        print(f" {num } is divisble by 3.")
+    else:
+        print(f" {num } isn't divisble by 3.")
+    if num%5 == 0:
+        print(f" {num } is divisble by 5.")
+    else:
+        print(f" {num } isn't divisble by 5.")
+    if num%3 and num%5 > 0:
+        print(f" {num } isn't divisble by 3 nor 5.")
+        
+result___________________________________________________________________________________________________________
+ 3 is divisble by 3.
+ 3 isn't divisble by 5.
+ 5 isn't divisble by 3.
+ 5 is divisble by 5.
+ 10 isn't divisble by 3.
+ 10 is divisble by 5.
+ 9 is divisble by 3.
+ 9 isn't divisble by 5.
+ 45 is divisble by 3.
+ 45 is divisble by 5.
+ 23 isn't divisble by 3.
+ 23 isn't divisble by 5.
+ 23 isn't divisble by 3 nor 5.
+ 90 is divisble by 3.
+ 90 is divisble by 5.
+ 7 isn't divisble by 3.
+ 7 isn't divisble by 5.
+ 7 isn't divisble by 3 nor 5.
+ 98 isn't divisble by 3.
+ 98 isn't divisble by 5.
+ 98 isn't divisble by 3 nor 5.
+ 89 isn't divisble by 3.
+ 89 isn't divisble by 5.
+ 89 isn't divisble by 3 nor 5.
+ 32 isn't divisble by 3.
+ 32 isn't divisble by 5.
+ 32 isn't divisble by 3 nor 5.
+ 54 is divisble by 3.
+ 54 isn't divisble by 5.
+ 65 isn't divisble by 3.
+ 65 is divisble by 5.
+ 76 isn't divisble by 3.
+ 76 isn't divisble by 5.
+ 76 isn't divisble by 3 nor 5.
+ 87 is divisble by 3.
+ 87 isn't divisble by 5.
+ 12 is divisble by 3.
+ 12 isn't divisble by 5.
+ 21 is divisble by 3.
+ 21 isn't divisble by 5.
+ 11 isn't divisble by 3.
+ 11 isn't divisble by 5.
+ 11 isn't divisble by 3 nor 5.
+ 22 isn't divisble by 3.
+ 22 isn't divisble by 5.
+ 22 isn't divisble by 3 nor 5.
+ 33 is divisble by 3.
+ 33 isn't divisble by 5.
+ 44 isn't divisble by 3.
+ 44 isn't divisble by 5.
+ 44 isn't divisble by 3 nor 5.
+ 55 isn't divisble by 3.
+ 55 is divisble by 5.
+ 66 is divisble by 3.
+ 66 isn't divisble by 5.
+ 77 isn't divisble by 3.
+ 77 isn't divisble by 5.
+ 77 isn't divisble by 3 nor 5.
+ 88 isn't divisble by 3.
+ 88 isn't divisble by 5.
+ 88 isn't divisble by 3 nor 5.
+ 99 is divisble by 3.
+ 99 isn't divisble by 5.
