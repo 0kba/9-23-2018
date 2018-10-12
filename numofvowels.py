f3c079bbdb69aee7d986603cfacde093f21d9366
0kba <m.okba@outlook.com>
2018-10-11 23:10:46 -0500

    using dictionnary to make the code smaller and works on many items

--- numofvowels.py
+++ numofvowels.py
@@ -2,7 +2,7 @@
 string = input("please write a statment")
 
 
-vowels = "a" + "e" + "i" + "o" + "u"
+vowels = {"a" : 0 , "e" : 0 , "i" : 0 ,  "o" : 0 , "u" : 0}
 vowelslist = []
 
 for char in string:
@@ -16,33 +16,13 @@ elif len(vowelslist) % 2 == 0:
 elif len(vowelslist) % 2 != 0:
     print(f"The text: '{string}' has an odd number of vowels")
 
-counti = 0
-counte = 0
-counta = 0
-counto = 0
-countu = 0
-
 for vowel in vowelslist:
-    if vowel == "i":
-        counti +=1
-    if vowel == "a":
-        counta +=1
-    if vowel == "e":
-        counte +=1
-    if vowel == "o":
-        counto +=1
-    if vowel == "u":
-        countu +=1                #counting how many we have on each vowels
-if counta !=0:
- print("a",counta)
-if counti != 0:
- print("i",counti)
-if counte != 0:
- print("e",counte)
-if counto != 0:
- print("o",counto)               # making sure the vowel in there before we print a count for it
-if countu != 0:
- print("u",countu)
+    if vowel in vowels:
+        vowels [vowel] += 1               #counting how many we have of each vowel
+
+for vo in vowels:
+    if vowels [vo] != 0:                   # making sure we have the vowel before print it
+     print(vo , vowels[vo])
 
 
 
@@ -52,8 +32,8 @@ if countu != 0:
 # please write a statment this is a test
 # The text: 'this is a test' has an even number of vowels
 # a 1
-# i 2
 # e 1
+# i 2
 # __________________
 # The text: 'we love programming' has an even number of vowels
 # a 1
