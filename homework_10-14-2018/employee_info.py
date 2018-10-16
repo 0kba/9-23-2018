94efcea344d9cc25d7a47529d960e9197969885b
0kba <m.okba@outlook.com>
2018-10-15 23:21:48 -0500

    created

---
+++ homework_10-14-2018/employee_info.py
@@ -0,0 +1,41 @@
+
+def writefromafile(filename):
+    try:
+        file = open(filename, "r")  # open and read the file in variable called " file "
+        line = file.readline()  # skipping the first line by reading it
+
+
+        list = []
+        item = ""
+
+        for lines in file:  # iterate over all the lines
+
+            for char in lines:  # iterate over each character in the line
+
+                if char == "," or char == "\n":  # if you found "," or "\n"(end of the line)
+                    list.append(item)  # append the variable item to the list
+                    item = ""  # then reset it to empty
+
+                else:
+                    item = item + char  # add every character in the variable "item"
+
+        list.append(item)  # now we have all items in the file in a list
+        if list == ['']:
+            print("your file is empty.")   # if list didn't capture anything that means the file is empty
+
+        while len(list) >= 4:  # once we have less than 4 items that means we are done
+
+            result = {"EmployeeId": list[0], "FullName": f"{list[2]}, {list[1]}", "Salary": f"${list[3]}"}
+            # we created a dictionary to capture every item in where it should be
+
+            print(result)  # print it right after u do that
+
+            del list[0:4]  # delete the first 4 items so we can move to the next line
+
+    except OSError:
+
+        print("Oops!  either you entered a wrong file name or the file doesn't exist.  Try again...")
+
+
+
+writefromafile("employee-info.txt")
