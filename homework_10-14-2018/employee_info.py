0182bbea677297f6792ac41a457139586568c2a8
0kba <m.okba@outlook.com>
2018-10-17 00:11:30 -0500

    divde the function to two function and make the code look more readable

--- homework_10-14-2018/employee_info.py
+++ homework_10-14-2018/employee_info.py
@@ -1,45 +1,57 @@
 
-def writefromafile(filename):
+def reading_file_to_a_list(filename):
+
     try:
-        file = open(filename, "r")  # open and read the file in variable called " file "
-        line = file.readline()  # skipping the first line by reading it
-
-
+        file = open(filename, "r")
+        line = file.readline()                    # skipping the first line by reading it
         list = []
         item = ""
 
-        for lines in file:  # iterate over all the lines
+        for lines in file:                        # iterate over all the lines
 
-            for char in lines:  # iterate over each character in the line
+            for char in lines:                    # iterate over each character in the line
 
-                if char == "," or char == "\n":  # if you found "," or "\n"(end of the line)
-                    list.append(item)  # append the variable item to the list
-                    item = ""  # then reset it to empty
-
+                if char == "," or char == "\n":   # if you found "," or "\n"(end of the line)
+                    list.append(item)             # append the variable item to the list
+                    item = ""                     # then reset it to empty
                 else:
-                    item = item + char  # add every character in the variable "item"
+                    item = item + char            # add every character in the variable "item"
 
-        list.append(item)  # now we have all items in the file in a list
-        if list == ['']:
-            print("your file is empty.")   # if list didn't capture anything that means the file is empty
+        list.append(item)                         # now we have all items in the file in a list
+        return list
 
-        while len(list) >= 4 :  # once we have less than 4 items that means we are done
+    except OSError:
+        return "Oops! the source file doesn't exist.  please try again..."
 
-            result = {"EmployeeId": list[0], "FullName": f"{list[2]}, {list[1]}", "Salary": f"${list[3]}"}
+# ____________________________________________________________________________________
+
+def sorting_list_to_dictionary (list):
+
+
+    if list == "Oops! the source file doesn't exist.  please try again...":
+        print("Oops! the source file doesn't exist.  please try again...")
+
+    elif list != ['']:
+        while len(list) >= 4:
+
+            result = {"Employee Id": list[0], "Full Name": f"{list[2]}, {list[1]}", "Salary": f"${list[3]}"}
             # we created a dictionary to capture every item in where it should be
 
-            print(result)  # print it right after u do that
+            print(result)
 
             del list[0:4]  # delete the first 4 items so we can move to the next line
 
-    except OSError:
 
-        print("Oops!  the file doesn't exist.  please try again...")
+    elif list == ['']:
+        print("the source file is empty")
 
+# __________________________________________________________________________________
 
+list = reading_file_to_a_list("employee-info.txt")
 
-writefromafile("employee-info.txt")
-writefromafile("jkfdjfhdk.txt")
+result = sorting_list_to_dictionary(list)
+
+print(result)
 
 # ______________________________________________________
 #   test results
@@ -47,17 +59,17 @@ writefromafile("jkfdjfhdk.txt")
 #
 # if writefromafile("employee-info.txt")
 #     result
-# {'EmployeeId': '1', 'FullName': 'Ali, Abdo', 'Salary': '$59533.54'}
-# {'EmployeeId': '2', 'FullName': 'Ali, Leeema', 'Salary': '$509533.54'}
-# {'EmployeeId': '3', 'FullName': 'hasan, mohamed', 'Salary': '$8376483'}
-# {'EmployeeId': '4', 'FullName': 'ibrahim, ahmed', 'Salary': '$9839.4'}
-#
+# {'Employee Id': '1', 'Full Name': 'Ali, Abdo', 'Salary': '$59533.54'}
+# {'Employee Id': '2', 'Full Name': 'Ali, Leeema', 'Salary': '$509533.54'}
+# {'Employee Id': '3', 'Full Name': 'hasan, mohamed', 'Salary': '$8376483'}
+# {'Employee Id': '4', 'Full Name': 'ibrahim, ahmed', 'Salary': '$9839.4'}
+# None
 #
 # if writefromafile("jkfdjfhdk.txt")
 # result
-# Oops!  either you entered a wrong file name or the file doesn't exist.  Try again...
+# Oops! the source file doesn't exist.  please try again...
 
 
-# if we tray an empty file
+# if writefromafile("empty.txt")
 #     result
-# your file is empty.
+# the source file is empty
