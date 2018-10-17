82c92c096ba5ca4572baed537322d55bed587ee2
0kba <m.okba@outlook.com>
2018-10-17 01:43:21 -0500

    make the code more readable didn't split it to more than one function

--- homework_10-14-2018/combine_infinite_lists.py
+++ homework_10-14-2018/combine_infinite_lists.py
@@ -1,38 +1,39 @@
-def combineinfinitelists (listofinfinitelists):
 
-    combinedlist = []        # empty list to add our result to it
+def combine_infinite_lists (list_of_infinite_lists):
 
-    for listornum in listofinfinitelists:       # iterating over the main list
+    combined_list = []
 
-        if type(combinedlist) == type(listornum):     # if it is a list go to the next line
-            for list in listornum:               # iterate over that particular list
+    for item in list_of_infinite_lists:
 
-                if type(combinedlist) == type(list):        # if it is a list go to the next line
-                    listofinfinitelists.append(list)   # if it is a list take it and add it to our main list
-# ( the benefit that now we have it just a list not a list inside a list inside a list and
-# it will go all the way to the end of our list which means we will iterate over it again later in our loop)
+        if type(combined_list) == type(item):                 # if it is a list go to the next line
 
-                else: combinedlist.append(list)  #  it means it is ready add it to our result list
+            for list in item:                                 # iterate over that particular list
 
-        else: combinedlist.append(listornum)    #  it means it is ready add it to our result list
+                if type(combined_list) == type(list):
+                    list_of_infinite_lists.append(list)              # if it is a list take it and add it to our main list
 
-    return combinedlist     # return our result list
+                else: combined_list.append(list)
 
+        else: combined_list.append(item)
 
-
-listofinfinitelists = [[1,2],[[2,3],[4,4],[[3,4],[[[3323,323],[323,434,434],[434433,434]],[4343,545]],[77,88]],[545,665]],[4,5]]
-printablelist =combineinfinitelists(listofinfinitelists)  # using the function on the above list
-
-
-listoflistsstrings =[[[1,2],[None],None,[3,4]],[[3,4],["hello","there"]]]
-printabletringlits = combineinfinitelists(listoflistsstrings)
+    return combined_list
 
 
 
-print(printablelist)
-print(printabletringlits)
+list_of_infinite_lists = [[[[[[[1,7]]],[[[[[[2,3]]]]]]]]]]
+printable_list =combine_infinite_lists(list_of_infinite_lists)
+
+
+
+strings_and_lists =[[[1,2],[None],None,[3,4]],[[3,4],["hello","there"]]]
+printable_string_list = combine_infinite_lists(strings_and_lists)
+
+
+
+print(printable_list)
+print(printable_string_list)
 
 # ________________________________________________________________________________
 # result
-# [1, 2, 4, 5, 2, 3, 4, 4, 545, 665, 3, 4, 77, 88, 4343, 545, 3323, 323, 323, 434, 434, 434433, 434]
-# [1, 2, 3, 4, 3, 4, 'hello', 'there']
+# [1, 7, 2, 3]
+# [None, 1, 2, None, 3, 4, 3, 4, 'hello', 'there']
