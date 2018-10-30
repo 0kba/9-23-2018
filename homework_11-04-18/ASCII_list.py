7e02fca83be871c95aabc248fa6459b01315dd71
0kba <m.okba@outlook.com>
2018-10-30 16:21:12 -0500

    divided to smaller function and adding more comments

--- homework_11-04-18/ASCII_list.py
+++ homework_11-04-18/ASCII_list.py
@@ -1,35 +1,55 @@
-def decimal_to_hex (decimal):
 
-    division_by_16 = decimal / 16
-    num_before_letter = int(division_by_16)
-    letter_definer = division_by_16 - num_before_letter
-    letters = {"A": 0.625, "B": 0.6875, "C": 0.75, "D": 0.8125, "E": 0.875, "F": 0.9375}
-
-    for key, value in letters.items():
-        if letter_definer == value:
-            return f"{num_before_letter}{key}"
-
-
-
-
+def decimal_to_hex_numbers_only (decimal) :      # (1) function does the hex equation for numbers only .. doesn't include hex letters and produce list
     remainder_list = []
+    place_value = 1                           # link to the source of the equation    https://www.permadi.com/tutorial/numDecToHex/
     while decimal != 0:
         remainder = decimal % 16
-        decimal = int(decimal / 16)
+        decimal = int(decimal / 16)        # reset decimal to be the int only of the divison result so next loop will divide the result not the original decimal
+        remainder = remainder * place_value
+        place_value *= 10
         remainder_list.append(remainder)
 
-    hex = 0
-    place_value = 1
-    for num in remainder_list:
-        num = num * place_value
-        place_value *= 10
-        hex = num + hex
+    return sum_of_list(remainder_list)
 
+
+def sum_of_list(remainder_list):             # (2) function to take care of this list by adding all numbers inside the list together
+    hex = 0
+    for num in remainder_list:
+        hex = num + hex
     return hex
 
 
-for char in range(128):
+def decimal_to_hex_letters (decimal) :       # (3) function returning hex letters
+
+    letters = {"A": 0.625, "B": 0.6875, "C": 0.75, "D": 0.8125, "E": 0.875, "F": 0.9375}       # dividing decimal by 16 gives us
+                                                     # integer number ( the number before the letter) and decimal number ( the value in the dictionary we created )
+    for key, value in letters.items():
+        if (decimal / 16) - int(decimal / 16) == value:
+            return f"{int(decimal / 16)}{key}"
+
+
+#________________________________________________________________________________________________________
+
+
+def decimal_to_hex (decimal):                # (4) main function (for hex problem) to call the past three function to convert any decimal to hex
+
+    positioner = (decimal / 16) - int(decimal / 16)    # positioner is a variable to check if the decimal will be
+                                                       # in the range of the hex letters or not(hex numbers)
+    if positioner >= 0.625 and positioner <= 0.9375 :
+
+        return decimal_to_hex_letters(decimal)
+
+
+    return decimal_to_hex_numbers_only (decimal)
+
+
+for char in range(128):                     # (5) print the ASCII table and using the decimal to hex function to produce hex in the table
     print(f"Decimal ({char}) is hex {decimal_to_hex(char)} and Keyboard Entry is ({chr(char)}) ")
+
+
+
+
+
 
 
 
