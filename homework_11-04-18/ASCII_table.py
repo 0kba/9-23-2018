8168af70716c5d696b2b41fd57d8a0ee55ca7214
0kba <m.okba@outlook.com>
2018-10-30 23:10:16 -0500

    deleting an empty file

--- homework_11-04-18/ASCII_table.py
+++ homework_11-04-18/ASCII_table.py
@@ -1,31 +1,50 @@
 
-def decimal_to_hex_decimal (decimal):
-    letters = {"0": 0 , "1": 0.0625 , "2": 0.125 , "3": 0.1875 , "4": 0.25 , "5": 0.3125 ,
-               "6": 0.375 , "7": 0.4375 , "8": 0.5 , "9": 0.5625 , "A": 0.625 , "B": 0.6875 ,
-               "C": 0.75 , "D": 0.8125 , "E": 0.875 , "F": 0.9375}                     # dividing decimal by 16 gives us
-                                                     # integer number ( the first number) and decimal number ( the value in the dictionary we created )
-                                                     # which will tell us which number or letter should we use.
-    for key, value in letters.items():
-        if (decimal / 16) - int(decimal / 16) == value:
-            return f"{hex(int(decimal / 16))}{key}"
+for char in range(401):
+    print(f"Decimal: ({char})    hex: ({hex(char)})     Keyboard Entry: ({chr(char)}) ")
 
 
-for char in range(401):                     # print the ASCII table and using the decimal to hexdecimal function to produce hex in the table
-    print(f"Decimal: ({char})    hex: ({decimal_to_hex_decimal(char)})     Keyboard Entry: ({chr(char)}) ")
+# results
+# Decimal: (397)    hex: (0x18d)     Keyboard Entry: (ƍ)
+# Decimal: (398)    hex: (0x18e)     Keyboard Entry: (Ǝ)
+# Decimal: (399)    hex: (0x18f)     Keyboard Entry: (Ə)
+# Decimal: (400)    hex: (0x190)     Keyboard Entry: (Ɛ)
+
+# ________________________________IGNORE WHAT IS UNDER THIS LINE______________________________________
+# ________________________________________________________________________________________________________
+#
+# def decimal_to_hex_decimal (decimal):
+#     letters = {"0": 0 , "1": 0.0625 , "2": 0.125 , "3": 0.1875 , "4": 0.25 , "5": 0.3125 ,
+#                "6": 0.375 , "7": 0.4375 , "8": 0.5 , "9": 0.5625 , "A": 0.625 , "B": 0.6875 ,
+#                "C": 0.75 , "D": 0.8125 , "E": 0.875 , "F": 0.9375}                     # dividing decimal by 16 gives us
+#                                                      # integer number ( the first number) and decimal number ( the value in the dictionary we created )
+#                                                      # which will tell us which number or letter should we use.
+#     for key, value in letters.items():
+#         if (decimal / 16) - int(decimal / 16) == value:
+#             return f"{hex(int(decimal / 16))}{key}"
+#
+#
+# for char in range(401):                     # print the ASCII table and using the decimal to hexdecimal function to produce hex in the table
+#     print(f"Decimal: ({char})    hex: ({decimal_to_hex_decimal(char)})     Keyboard Entry: ({chr(char)}) ")
+#
+
+________________________________________________________________________________
 
 
-# __________________TEST CASES RESULTS______________________________________
-# Decimal: (0)    hex: (00)     Keyboard Entry: ( )
-# Decimal: (1)    hex: (01)     Keyboard Entry: ()
-# Decimal: (2)    hex: (02)     Keyboard Entry: ()
-# Decimal: (3)    hex: (03)     Keyboard Entry: ()
-# Decimal: (4)    hex: (04)     Keyboard Entry: ()
-# .etc
-# Decimal: (120)    hex: (78)     Keyboard Entry: (x)
-# Decimal: (121)    hex: (79)     Keyboard Entry: (y)
-# Decimal: (122)    hex: (7A)     Keyboard Entry: (z)
-# Decimal: (123)    hex: (7B)     Keyboard Entry: ({)
-# Decimal: (124)    hex: (7C)     Keyboard Entry: (|)
-# Decimal: (125)    hex: (7D)     Keyboard Entry: (})
-# Decimal: (126)    hex: (7E)     Keyboard Entry: (~)
-# Decimal: (127)    hex: (7F)     Keyboard Entry: ()
+# def decimal_to_hex_numbers_only (decimal) :      # (1) function does the hex equation for numbers only .. doesn't include hex letters and produce list
+#     remainder_list = []
+#     place_value = 1                           # link to the source of the equation    https://www.permadi.com/tutorial/numDecToHex/
+#     while decimal != 0:
+#         remainder = decimal % 16
+#         decimal = int(decimal / 16)        # reset decimal to be the int only of the divison result so next loop will divide the result not the original decimal
+#         remainder = remainder * place_value
+#         place_value *= 10
+#         remainder_list.append(remainder)
+#
+#     return sum_of_list(remainder_list)
+#
+#
+# def sum_of_list(remainder_list):             # (2) function adding all numbers inside the list together
+#     hex = 0
+#     for num in remainder_list:
+#         hex = num + hex
+#     return hex
