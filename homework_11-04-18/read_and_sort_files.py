755b2e6bec755e92dfc061f74f87a74a6b8fb0ba
0kba <m.okba@outlook.com>
2018-11-04 21:48:19 -0600

    update from the class

--- homework_11-04-18/read_and_sort_files.py
+++ homework_11-04-18/read_and_sort_files.py
@@ -1,60 +1,53 @@
-from operator import itemgetter as it
+from operator import attrgetter
 import sys
+from datetime import date
 
-def read_and_sort_files (file, LastName):
-    file = open(file, "r")
-    line = file.readline()  # skipping the first line by reading it
-    list_of_words = []
-    item = ""
+class Student:
+    def __init__(self, firstname, lastname,dob):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.birthdate = self.__parse_date__(dob)
 
-    for lines in file:  # iterate over all the lines
-        item = read_each_line(item, lines, list_of_words)
+    def print(self):
+        print(f"{self.firstname} {self.lastname} {self.birthdate}")
 
-    list_of_words.append(item)  # now we have all items in the file in a list_of_words
-
-    final = fil_list_to_list_of_dic(list_of_words)
-
-    return sorting_by_key(LastName, final)
-
-
-def read_each_line(item, lines, list):
-    for char in lines:  # iterate over each character in the line
-
-        item = read_each_word(char, item, list)
-    return item
+    def __parse_date__(self, dob):
+        tokens = dob.split("-")
+        return date(int(tokens[0]), int(tokens[1]), int(tokens[2]))
 
 
-def read_each_word(char, item, list):
-    if char == "," or char == "\n":  # if you found "," or "\n"(end of the line)
-        list.append(item)  # append the variable item to the list
-        item = ""  # then reset it to empty
-    else:
-        item = item + char  # add every character in the variable "item"
-    return item
+def read_students_file (filename):
+    file = open(filename, "r")
+    file.readline()  # skipping the first line by reading it
+    students = []
 
+    for line in file:  # iterate over all the line
+        student = create_student_from_line(line)
+        students.append(student)  # now we have all items in the file in a list_of_words
 
-def fil_list_to_list_of_dic(list):
-    final = []
-    while len(list) >= 3:
-        final.append({"FirstName": list[0], "LastName": list[1], "DateOfBirth": f"{list[2]}"})   # we created a dictionary to capture every item in where it should be
-                                                                                                 # then append it to a list
-        del list[0:3]                                                                            # delete the first 4 items so we can move to the next line
-    return final
+    file.close()
+    return students
 
+def create_student_from_line(line):
+    tokens = line.split(",")
+    student = Student(tokens[0], tokens[1], tokens[2])
+    return  student
 
 def sorting_by_key(LastName, final):
     final.sort(key=it(LastName))  # print the whole list of dic(of students) sorted by the key gaven
     return final
-
 
 def main():
     if len(sys.argv) < 3:
         print("Pass a file name and a sorting key for example (student_data.txt FirstName)")
         return
     try:
-        file_name = sys.argv[1]    # the file name
-        sorting_key = sys.argv[2]    # sorting key
-        print( read_and_sort_files(file_name,sorting_key))
+        file_name = sys.argv[1]
+        sorting_key = sys.argv[2]
+        students = read_students_file(file_name)
+        students.sort(key=attrgetter(sorting_key))
+        for student in students:
+            student.print()
         return
 
     except KeyError:
@@ -63,31 +56,6 @@ def main():
     except FileNotFoundError:
         print(f"{file_name} doesn't exist please try a different file", file=sys.stderr)
         return
-
-#_______________________________RESULTS______________________________________
-# C:\Users\naweh\python\homework_11-04-18>read_and_sort_files.py student_data.txt LastName
-# [{'FirstName': 'Juha', 'LastName': 'Abdo', 'DateOfBirth': '8/1/200'},
-#  {'FirstName': 'Magas', 'LastName': 'Kisra', 'DateOfBirth': '7/2/1999'},
-#  {'FirstName': 'Ali', 'LastName': 'Saleh', 'DateOfBirth': '5/5/2005'},
-#  {'FirstName': 'Manal', 'LastName': 'Saleh', 'DateOfBirth': '5/9/2004'}]
-
-#___________________
-
-# C:\Users\naweh\python\homework_11-04-18>read_and_sort_files.py
-# Pass a file name and a sorting key for example (student_data.txt FirstName)
-
-#_____________________
-
-#C:\Users\naweh\python\homework_11-04-18>read_and_sort_files.py student_data.txt kfj43
-# kfj43 is not a valid Entry the key has to be written in one of those ways(FirstName,LastName or DateOfBirth)
-
-#____________________
-
-# C:\Users\naweh\python\homework_11-04-18>read_and_sort_files.py dkjhd87 LastName
-# dkjhd87 doesn't exist please try a different file
-
-
-
 
 if __name__ == '__main__':
     main()
