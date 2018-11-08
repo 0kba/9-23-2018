8a8e95b61ea72ed3bf5f7053d77fbc92ff98fc4d
0kba <m.okba@outlook.com>
2018-11-07 19:56:40 -0600

    making it look better

--- student_class.py
+++ student_class.py
@@ -1,12 +1,19 @@
 class Student:
-	def __init__(self, firstname, lastname, gradeLevel):
-             self.firstname = firstname
-             self.lastname = lastname
-             self.grade_level = gradeLevel
-	
-	def get_whole_name(self):
-		return f"{self.lastname}, {self.firstname}"
+    def __init__(self, firstname, lastname, gradeLevel):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.grade_level = gradeLevel
+
+    def display(self):
+        print(f"{self.lastname}, {self.firstname}  " , self.grade_level)
+
+    # def get_whole_name(self):
+    #     return f"{self.lastname}, {self.firstname}"
 
 
-studentA = Student('Abdo', 'Ali', 5)
-print(studentA.get_whole_name())
+d = Student('Abdo', 'Ali', 544), Student('Ab', 'Al', 544),Student('A', 'A', 533),Student('Abd', 'f', 54)
+
+
+for i in d:
+    print(i.display())
+
