8523534e62558470a5fac2bafa15cf7b5a9bee3d
0kba <m.okba@outlook.com>
2018-11-14 22:16:19 -0600

    fixinf a condition combining issue + adding test cases results

--- homework_11-18-18/GCD.py
+++ homework_11-18-18/GCD.py
@@ -15,11 +15,8 @@ def GCD_algorithm(first_number, second_number):
     return first_number
 
 def main():
-    if len(sys.argv) < 3:
-        print("Pass two numbers separated by space to get their GCD")
-        return
-    elif len(sys.argv) > 3:
-        print("please enter just two numbers not more")
+    if 3 < len(sys.argv) < 3:
+        print("Pass just two numbers separated by space to get their GCD")
         return
     try:
         first_number = int(sys.argv[1])
@@ -29,6 +26,27 @@ def main():
     except ValueError:
         print(f"one or both of ur numbers are not valid numbers", file=sys.stderr)
         return
+
+# __________________________________________TEST CASES_____________________________________________
+# C:\python\homework_11-18-18>GCD.py 270 192
+# 6
+#
+# C:\python\homework_11-18-18>GCD.py -23 323
+# -1
+#
+# C:\python\homework_11-18-18>GCD.py 270 uye
+# one or both of ur numbers are not valid numbers
+#
+# C:\python\homework_11-18-18>GCD.py 270,873
+# one or both of ur numbers are not valid numbers
+# C:python\homework_11-18-18>GCD.py 0 0
+# 0
+#
+# C:python\homework_11-18-18>GCD.py 0 987
+# 987
+#
+# C:python\homework_11-18-18>GCD.py 654 0
+# 654
 
 if __name__ == '__main__':
     main()
