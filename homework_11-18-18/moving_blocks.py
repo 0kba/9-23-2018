cfa580d44de87efc12c2226080784f2c153f60e4
0kba <m.okba@outlook.com>
2018-11-15 00:17:14 -0600

    holding on moving blocks code for now

--- homework_11-18-18/moving_blocks.py
+++ homework_11-18-18/moving_blocks.py
@@ -14,21 +14,3 @@
 # take the smallest to the source
 # take the medium to the dest
 # take the smallest to the dest
-
-def move(blocks,source,temp,destination):
-    if blocks == 0:
-        return
-    elif blocks ==1:
-        return print(f"move {blocks} to {destination}")
-    elif blocks ==2:
-        return moving_two_blocks(blocks,temp,destination)
-
-
-def moving_two_blocks(blocks,temp,destination):
-    print(f"move {blocks-1} to {temp} ")
-    print(f"move {blocks} to {destination}")
-    print(f"move {blocks-1} to {destination}")
-    return
-
-
-print(move(2,"a","b","c"))
