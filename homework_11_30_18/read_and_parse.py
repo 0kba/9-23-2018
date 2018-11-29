c4d1fa8190237d80386be50dc4f7ee4abfd201c1
0kba <m.okba@outlook.com>
2018-11-28 23:33:00 -0600

    fixing some errors

--- homework_11_30_18/read_and_parse.py
+++ homework_11_30_18/read_and_parse.py
@@ -1,3 +1,4 @@
+import datetime
 class Product:
     def __init__(self, ProductId, ProductName, Category, Price):
         self.ProductId = ProductId
@@ -5,8 +6,8 @@ class Product:
         self.Category = Category
         self.Price = Price
 
-    def print_whole (self):
-        print (f"{self.ProductId} {self.ProductName} {self.Category} {self.Price}")
+    def __repr__(self):
+        return (f"{self.ProductId} {self.ProductName} {self.Category} {self.Price}")
 
 
 class Order:
@@ -18,9 +19,9 @@ class Order:
 
     def __parse_date__(self,OrderDate):
         token = OrderDate.split("-")
-        return int(token[1]) , int(token[2]) , int(token[0])
+        return datetime.date(int(token[0]), int(token[1]), int(token[2]))
 
-    def print_whole(self):
+    def __repr__(self):
         return (f"{self.OrderId} {self.OrderDate} {self.Quantity} {self.ProductId}")
 
 
@@ -37,26 +38,51 @@ def read_contents_to_list(file_name):
     opened_file.close()
     return file_contents
 
+
 def parsing_to_list(file_name):
     file = read_contents_to_list(file_name)
     products_list = []
 
     for line in file:
         items = line.split(",")
-        products_list.append(Product(items[0],items[1],items[2],items[3]).print_whole())
+        products_list.append(Product(items[0],items[1],items[2],items[3]))
+
     return products_list
 
-def parsing_to_dic(file_name):
+
+def parsing_to_dictionary(file_name):
     file = read_contents_to_list(file_name)
     orders_dictionary = {}
     for line in file:
         items = line.split(",")
         if items[3] not in orders_dictionary:
-          orders_dictionary[items[3]] = [Order(items[0],items[1],items[2],items[3]).print_whole()]
+          orders_dictionary[items[3]] = [Order(items[0],items[1],items[2],items[3])]
         else:
-            orders_dictionary[items[3]].append([Order(items[0],items[1],items[2],items[3]).print_whole()])
+            orders_dictionary[items[3]].append([Order(items[0],items[1],items[2],items[3])])
     return orders_dictionary
 
 
+
+
+
+
+
+
+def list_orders_using_ProudctId(dictionary,ProductId):
+    for key in dictionary:
+        if key == ProductId:
+            return dictionary[key]
+
+def search_by_name(products_list,searching_name):
+    for product in products_list:
+        if searching_name in product:
+            return product
+
+
+
+
+
 print(parsing_to_list("product.txt"))
-print(parsing_to_dic("order.txt"))
+print(parsing_to_dictionary("order.txt"))
+
+
