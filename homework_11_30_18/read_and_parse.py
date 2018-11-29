f34fe299887cd6438ebe2fb870c83670b372041e
0kba <m.okba@outlook.com>
2018-11-29 14:50:25 -0600

    all known errors until now has been fixed

--- homework_11_30_18/read_and_parse.py
+++ homework_11_30_18/read_and_parse.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+
 class Product:
     def __init__(self, ProductId, ProductName, Category, Price):
         self.ProductId = ProductId
@@ -7,10 +8,8 @@ class Product:
         self.Category = Category
         self.Price = Price
 
-
     def __repr__(self):
         return (f"{self.ProductId} {self.ProductName} {self.Category} {self.Price}")
-
 
 class Order:
     def __init__(self, OrderId, OrderDate, Quantity, ProductId):
@@ -19,10 +18,9 @@ class Order:
         self.Quantity = Quantity
         self.ProductId = ProductId
 
-
     def __parse_date__(self,OrderDate):
         token = OrderDate.split("-")
-        return datetime.datetime(int(token[0]), int(token[1]), int(token[2]))
+        return datetime.datetime.strptime( f'{token[0]}-{token[1]}-{token[2]}','%Y-%m-%d').date()
 
     def __repr__(self):
         return (f"{self.OrderId} {self.OrderDate} {self.Quantity} {self.ProductId}")
@@ -39,16 +37,13 @@ def read_contents_to_list(file_name):
     opened_file.close()
     return file_contents
 
-
 def parsing_to_list(file_name):
     file = read_contents_to_list(file_name)
     products_list = []
-
     for line in file:
         items = line.split(",")
         products_list.append(Product(items[0],items[1],items[2],items[3]))
     return products_list
-
 
 def parsing_to_dictionary(file_name):
     file = read_contents_to_list(file_name)
@@ -59,22 +54,21 @@ def parsing_to_dictionary(file_name):
           orders_dictionary[items[3]] = [Order(items[0],items[1],items[2],items[3])]
         else:
             orders_dictionary[items[3]].append([Order(items[0],items[1],items[2],items[3])])
-
     return orders_dictionary
-
-
-
-
 
 def list_orders_using_ProudctId(dictionary,ProductId):
     for key in dictionary:
         if key == ProductId:
             return dictionary[key]
+        else:
+            return "not found please try again"
 
 def search_by_name(products_list,searching_name):
     for product in products_list:
-        if searching_name == product.ProductName:
+        if searching_name.lower() == product.ProductName.lower():
             return product
+        else:
+            return "not found please try again"
 
 def list_by_catagory(products_list):
     catagory_dictionary = {}
@@ -85,20 +79,18 @@ def list_by_catagory(products_list):
             catagory_dictionary[product.Category].append([product])
     return catagory_dictionary
 
-
-
 def combine_list_of_lists (main_list):
     result_list = []
     for item in main_list:
-        if type(result_list) == type(item):                 # if it is a list go to the next line
+        if type(result_list) == type(item):
             peel_list_layer(result_list, item, main_list)
         else: result_list.append(item)
     return result_list
 
 def peel_list_layer(result_list, item, main_list):
-    for list in item:  # iterate over that particular list
+    for list in item:
         if type(result_list) == type(list):
-            main_list.append(list)  # if it is a list take it and add it to our main list
+            main_list.append(list)
         else:
             result_list.append(list)
 
@@ -117,42 +109,43 @@ def orders_between_2dates(orders_dictionary,date1,date2):
         date1 = date2
         date2 = temp
     for order in orders:
-        if date1 >= order.OrderDate >= date2:
+        if date2 <= order.OrderDate <= date1 :
             result.append(order)
+    if result == []:
+        return "no orders have been found please try again"
     return result
+
+def import_2dates_from_user(orders_dictionary):
+    print("please enter the first date in this format Y-M-D then press Enter")
+    date1 = input(datetime.datetime)
+    date1 = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
+    print("please enter the second date in this format Y-M-D then press Enter")
+    date2 = input(datetime.datetime)
+    date2 = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
+    return (orders_between_2dates(orders_dictionary, date1, date2))
 
 def options_output (first_file,second_file,option):
     products_list = parsing_to_list(first_file)
     orders_dictionary = parsing_to_dictionary(second_file)
     if option == '1':
        return (list_by_catagory(products_list))
-
     elif option == '2':
         ProductId = input(print("please enter the ProductId"))
         return (list_orders_using_ProudctId(orders_dictionary, ProductId))
-
     elif option == '3':
         searching_name = input(print("please enter the name of the product you want to search for"))
         return (search_by_name(products_list, searching_name))
-
     elif option == '4':
-        date1 = input(datetime.date )
-        date1 = datetime.datetime.strptime(date1,'%Y,%M,%d')
-        date2 = input(datetime.date)
-        date2 = datetime.datetime.strptime(date2, '%Y,%M,%d')
-
-        return (orders_between_2dates(orders_dictionary,date1,date2))
+        return import_2dates_from_user(orders_dictionary)
 
 def __main__():
     if len(sys.argv) < 3:
         print("Please write two valid files names separated by space then you will get options")
         return
-
     try:
         first_file = sys.argv[1]
         second_file = sys.argv[2]
         option = None
-
         while option != '0':
             option = input(print(f"""please Enter: 
             0 - to quit
@@ -164,12 +157,9 @@ def __main__():
                 print(options_output(first_file, second_file, option))
             elif option != '0':
                 print("that isn't a valid entry please try again")
-
         if option == '0':
             print("bye bye")
             return
-
-
 
     except IndentationError:
         print(f"{sys.argv} is not a valid entry", file=sys.stderr)
@@ -177,9 +167,6 @@ def __main__():
     except FileNotFoundError:
         print(f"one or both of your files doesn't exist", file=sys.stderr)
         return
-
-
-
 
 
 __main__()
